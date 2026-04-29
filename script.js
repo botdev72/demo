b8b1d66dda5bc71cc1d61d2b9c41451f765d94e7
BAYU RIZKI ZULFIKRI <146010250+botdev72@users.noreply.github.com>
2026-04-30 01:12:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const reveals=document.querySelectorAll('.reveal');
-const observer=new IntersectionObserver(entries=>{
- entries.forEach(e=>{
-  if(e.isIntersecting){e.target.classList.add('active')}
- })
-});
-reveals.forEach(r=>observer.observe(r));
